be3407193ccd714c9a71d21fa6bc0e252c8a1805
tclewis91 <tclewis91@gmail.com>
2015-05-12 08:33:52 -0500

    corrections

--- simplified-profile.rb
+++ simplified-profile.rb
@@ -1,17 +1,19 @@
-Puts "What is your name?"
+puts "What is your name?"
 name=gets.chomp
-Print "Where were you born (city, state)?"
+print "Where were you born (city, state)?"
 born=gets.chomp
-Print "How old are you?"
+print "How old are you?"
 age=gets.chomp
-Print "What is your username?"
-user_name=gets.chomp.
-Print "What is your favorite food?"
+print "What is your username?"
+user_name=gets.chomp
+print "What is your favorite food?"
 food1=gets.chomp
-Print "Do you like to eat anything with that?"
+print "Do you like to eat anything with that?"
 food2=gets.chomp
-Puts "What are three other foods that you like to eat?"
-food= [item1, item2, item3]
-food=gets.chomp
-Print "So, you're name is #{name}; you're user_name is #{user_name}. You were born in #{born}.
-Your favorite food is #{favorite} and you like to eat it with #{food2}. However, you also really like to eat #{food}.
+puts "What are three other foods that you like to eat?"
+food3=gets.chomp
+food4=gets.chomp
+food5=gets.chomp
+food=[food3, food4,food5]
+print "So, you're name is #{name}; you're user_name is #{user_name}. You were born in #{born}.
+Your favorite food is #{food1} and you like to eat it with #{food2}. However, you also really like to eat #{food}."
